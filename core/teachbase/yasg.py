5d856dc29f217726c495dc12a9f37fa84b5f8e9b
Nikolaev Valentin <val8673@gmail.com>
2023-03-02 22:00:48 +0100

    fix swagger

--- core/teachbase/yasg.py
+++ core/teachbase/yasg.py
@@ -6,7 +6,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="physical API",
+        title="Teachbase API",
         default_version="v1",
         description="API post",
         license=openapi.License(name="BSD License"),
